4aa05735937cfb9fa883f1cce2ce1f63e9a512b0
DESKTOP-DGT8C1G\lhn <1085436468@qq.com>
2023-04-03 22:59:36 +0800

    首次提交

---
+++ src/views/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>你好！</div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Home',
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
