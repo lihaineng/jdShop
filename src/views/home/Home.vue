8dce9775da92f09756aca30ad19a3942cbfa8e3b
lihn49 <lihn1l2H3nE@3>
2023-04-29 16:45:08 +0800

    首页完成

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,41 +1,25 @@
 <template>
     <div>
       <div class="wrapper">
-        <div class="position">
-          <span class="iconfont position__icon">&#xe619;</span>
-          北京理工大学国防科技园2号楼10层
-          <span class="iconfont position_notice">&#xe60b;</span>
-        </div>
+        <StaticPart/>
+        <Nearby />
       </div>
-      <div class="docker">
-        <div class="docker__item docker__item--active">
-          <div class="iconfont">&#xe6f3;</div>
-          <div class="docker__title">首页</div>
-        </div>
-        <div class="docker__item">
-          <div class="iconfont">&#xe7e5;</div>
-          <div class="docker__title">购物车</div>
-          </div>
-        <div class="docker__item">
-          <div class="iconfont">&#xe61e;</div>
-          <div class="docker__title">订单</div>
-        </div>
-        <div class="docker__item">
-          <div class="iconfont">&#xe660;</div>
-          <div class="docker__title">我的</div>
-        </div>
-      </div>
+      <Docker />
     </div>
   </template>
 
   
 <script>
+import Docker from './Docker.vue'
+import Nearby from './Nearby.vue'
+import StaticPart from './StaticPart.vue'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {StaticPart, Docker, Nearby}
 }
 </script>
   
-  <style lang="scss" scoped>
+<style lang="scss" scoped>
   @import '../../style/viriables.scss';
   @import '../../style/mixins.scss';
   .wrapper {
@@ -65,33 +49,5 @@ export default {
     }
     color: $content-fontcolor;
   }
-  .docker {
-    display: flex;
-    box-sizing: border-box;
-    position: absolute;
-    padding: 0 .18rem;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    height: .49rem;
-    border-top: .01rem solid #F1F1F1;
-    color: $content-fontcolor;
-    &__item {
-      flex: 1;
-      text-align: center;
-      .iconfont {
-        margin: .07rem 0 .02rem 0;
-        font-size: .18rem;
-      }
-      &--active {
-        color: #1FA4FC;
-      }
-    }
-    &__title {
-      font-size: .2rem;
-      transform: scale(.5, .5);
-      transform-origin: center top;
-    }
-  }
-  </style>
+</style>
   
